9fe7b3290eaadcc8587da1292721e80ecf4edd93
nikspolaroid <nikhilsviswam@gmail.com>
2026-06-17 13:49:48 +0200

    [phase-3] PLC tank control program running in OpenPLC simulator

---
+++ plc/tank_control/build/OpenPLC Simulator/precompile/obj/arduino_runtime_glue.d
@@ -0,0 +1,37 @@
+/Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/precompile/obj/arduino_runtime_glue.o: \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/precompile/sources/arduino_runtime_glue.cpp \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/arduino_runtime_glue.h \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/openplc.h \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/generated.hpp \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/iec_types.hpp \
+ /var/folders/n7/gm0gtf1d66n78w5l4fny18dc0000gn/T/openplc-avr-libstdcpp-4.2.6/include/cstdint \
+ /var/folders/n7/gm0gtf1d66n78w5l4fny18dc0000gn/T/openplc-avr-libstdcpp-4.2.6/include/cstddef \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/iec_var.hpp \
+ /var/folders/n7/gm0gtf1d66n78w5l4fny18dc0000gn/T/openplc-avr-libstdcpp-4.2.6/include/type_traits \
+ /var/folders/n7/gm0gtf1d66n78w5l4fny18dc0000gn/T/openplc-avr-libstdcpp-4.2.6/include/utility \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/iec_array.hpp \
+ /var/folders/n7/gm0gtf1d66n78w5l4fny18dc0000gn/T/openplc-avr-libstdcpp-4.2.6/include/array \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/iec_fault.hpp \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/iec_located.hpp \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/iec_std_lib.hpp \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/iec_traits.hpp \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/iec_retain.hpp \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/iec_ptr.hpp \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/iec_string.hpp \
+ /var/folders/n7/gm0gtf1d66n78w5l4fny18dc0000gn/T/openplc-avr-libstdcpp-4.2.6/include/cstdio \
+ /var/folders/n7/gm0gtf1d66n78w5l4fny18dc0000gn/T/openplc-avr-libstdcpp-4.2.6/include/cstring \
+ /var/folders/n7/gm0gtf1d66n78w5l4fny18dc0000gn/T/openplc-avr-libstdcpp-4.2.6/include/cstdlib \
+ /var/folders/n7/gm0gtf1d66n78w5l4fny18dc0000gn/T/openplc-avr-libstdcpp-4.2.6/include/algorithm \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/iec_wstring.hpp \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/iec_time.hpp \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/iec_date.hpp \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/iec_dt.hpp \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/iec_tod.hpp \
+ /var/folders/n7/gm0gtf1d66n78w5l4fny18dc0000gn/T/openplc-avr-libstdcpp-4.2.6/include/cmath \
+ /var/folders/n7/gm0gtf1d66n78w5l4fny18dc0000gn/T/openplc-avr-libstdcpp-4.2.6/include/chrono \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/iec_enum.hpp \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/iec_memory.hpp \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/iec_pointer.hpp \
+ /var/folders/n7/gm0gtf1d66n78w5l4fny18dc0000gn/T/openplc-avr-libstdcpp-4.2.6/include/string \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/debug_dispatch.hpp \
+ /Users/nikhilsviswam/Documents/smart-bottling-line/plc/tank_control/build/OpenPLC\ Simulator/src/debug_table.hpp
